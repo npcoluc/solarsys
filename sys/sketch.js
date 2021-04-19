a0796a332c68a7f40df2560cccd66b859b2925eb
apothukuchipq <Ashwin.Pothukuchi@proquest.com>
2021-04-18 23:12:12 -0400

    basics with iframe

--- sys/sketch.js
+++ sys/sketch.js
@@ -48,7 +48,7 @@ function setup() {
 
 
 function draw() {
-  background(0, 0, 10)
+  background(236, 68, 10)
   for(let i = 0;i<particles.length;i++) {
     particles[i].createParticle();
     particles[i].moveParticle();
